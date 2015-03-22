0c3ac04c797c48fa4d5c2866ecb61d556ffd3437
p736m3dmht7t <p736m3dmht7t@users.noreply.github.com>
2015-03-22 14:08:14 -0700

    second commit.  updated comments.

--- run_analysis.R
+++ run_analysis.R
@@ -7,6 +7,7 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average
 #    of each variable for each activity and each subject.
 #
+
 # 1a. Read the train data set
 setwd('~/classes/Getting and Cleaning Data/Week 3 Assignment/')
 train.data.set<-read.table('data/UCI HAR Dataset/train/X_train.txt')
@@ -17,6 +18,7 @@ names(train.subjects)<-'subject'
 train.y       <-read.table('data/UCI HAR Dataset/train/y_train.txt')
 names(train.y)<-'y'
 train.data.set<-cbind(train.data.set,train.subjects,train.y)
+
 # 1b. Read the test data set
 test.data.set<-read.table('data/UCI HAR Dataset/test/X_test.txt')
 names(test.data.set)<-vars[,2]
@@ -25,27 +27,34 @@ names(test.subjects)<-'subject'
 test.y       <-read.table('data/UCI HAR Dataset/test/y_test.txt')
 names(test.y)<-'y'
 test.data.set<-cbind(test.data.set,test.subjects,test.y)
+
 # 1c. Merge the train and test data sets
 data.set<-rbind(train.data.set,test.data.set,all=TRUE)
 rm(train.data.set,train.subjects,train.y,test.data.set,test.subjects,test.y)
+
 # 2a. Identify the columns that contain means or standard deviations
 index<-grepl('mean',vars[,2],ignore.case=TRUE) | grepl('std',vars[,2],ignore.case=TRUE)
 index<-index & !grepl('angle\\(',vars[,2],ignore.case=TRUE)
+
 # 2b. Include subject and y columns
 index<-c(index,TRUE,TRUE)
 data.set<-data.set[,index]
+
 #3a.  Convert 'y' column to descriptive activities
 activity<-c('WALKING','WALKING_UPSTAIRS','WALKING_DOWNSTAIRS','SITTING','STANDING','LAYING')
 data.set$activity<-activity[data.set$y]
 index<-!names(data.set)=='y'
 data.set<-data.set[,index]
+
 #4a.  Appropriately label the columns of the data set
 data.set$subject<-as.factor(data.set$subject)
 data.set$activity<-as.factor(data.set$activity)
 rm(index,vars,activity)
+
 #5a.  Create a tidy data set of the means of the columns
 tidy.data.set<-aggregate(.~subject+activity,data=data.set,mean)
 write.table(tidy.data.set,'data/UCI HAR Dataset/tidy_data_set.txt',row.names=FALSE)
+
 #6.  Generate a code book from the tidy data set
 n<-dim(tidy.data.set)[2]
 code.book<-data.frame(variable_name=character(n),
